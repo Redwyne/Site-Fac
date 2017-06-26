259146cac8de30f703de41ed33c29a2ed25c2a38
michm <michel.kamel@hotmail.com>
2017-06-26 17:33:27 +0200

    Final

---
+++ js/Candidature.js
@@ -0,0 +1,11 @@
+/**
+ * Created by michm on 26/06/2017.
+ */
+function ouvrirnav(){
+    document.getElementById("nav").style.height="100%";
+
+}
+
+function fermernav(){
+    document.getElementById("nav").style.height="0%";
+}
---
+++ js/Programmes.js
@@ -0,0 +1,45 @@
+/**
+ * Created by michm on 25/06/2017.
+ */
+function Opennav() {
+    document.getElementById("dropdown-content").style.padding="50% 200% 50% 200%";
+    document.getElementById("Navigateur").style.height="100%";
+
+
+
+}
+
+function fermer () {
+    document.getElementById("dropdown-content").style.padding="0 0 0 0";
+    document.getElementById("Navigateur").style.height= "0%";
+
+}
+
+function Opennavm() {
+    document.getElementById("dropdown-contentm").style.padding="50% 200% 50% 200%";
+    document.getElementById("Navigateurm").style.height="100%";
+
+
+
+}
+
+function fermerm () {
+    document.getElementById("dropdown-contentm").style.padding="0 0 0 0";
+    document.getElementById("Navigateurm").style.height= "0%";
+
+}
+
+
+function Opennavd() {
+    document.getElementById("dropdown-contentd").style.padding="50% 200% 50% 200%";
+    document.getElementById("Navigateurd").style.height="100%";
+
+
+
+}
+
+function fermerd () {
+    document.getElementById("dropdown-contentd").style.padding="0 0 0 0";
+    document.getElementById("Navigateurd").style.height= "0%";
+
+}
